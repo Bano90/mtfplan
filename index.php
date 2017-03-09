5b9816afc02db8d2ab73023f7cbaf17a99167780
Albano <502678184@logon.ds.ge.com>
2017-03-09 14:54:46 +0100

    +fields

--- index.php
+++ index.php
@@ -39,8 +39,19 @@
          $db=new \DB\Jig('plans',\DB\Jig::FORMAT_JSON);
          $mapper=new \DB\Jig\Mapper($db,'plans.json');
 
-         $mapper->id=$data->id;
-         $mapper->date=$data->date;
+         $mapper->id=$data->id; //azonosító
+         $mapper->date=$data->date; //dátum
+         $mapper->projectname=$data->projectname; //projekt neve
+         $mapper->type=$date->type; //típus
+         $mapper->amountshift1=$data->amountshift1; // tervezett darab szám reggeles szak
+         $mapper->amountshift2=$data->amountshift2; // tervezett darab szám délutános szak
+         $mapper->amountshift3=$data->amountshift3; // tervezett darab szám éjszakás szak
+         $mapper->description=$data->description; // részletes leírás
+         $mapper->attempt=$data->attempt; //próbálkozás
+         $mapper->whogo=$data->whogo; //ki megy
+         $mapper->where=$data->where; //hova megy
+         $mapper->period=$data->period; //mennyi ideig
+         $mapper->person=$data->person; //hány főre van szükség
          $mapper->save();
 
          echo "OK";
@@ -57,7 +68,17 @@
          $mapper=new \DB\Jig\Mapper($db,'plans.json');
          $plan=$mapper->load(Array('@id=?',$params['id']));
 
-         //$plan->date=$data->date;
+         $plan->projectname=$data->projectname;
+         $plan->type=$data->type;
+         $plan->amountshift1=$data->amountshift1;
+         $plan->amountshift2=$data->amountshift2;
+         $plan->amountshift3=$data->amountshift3;
+         $plan->description=$data->description;
+         $plan->attempt=$data->attempt;
+         $plan->whogo=$data->whogo;
+         $plan->where=$data->where;
+         $plan->period=$data->period;
+         $plan->person=$data->person;
          $plan->save();
 
          echo "OK";
