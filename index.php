d5d9c50166da3ac280fedc46977137da90ac53df
Albano <502678184@logon.ds.ge.com>
2017-03-29 10:30:26 +0200

    jav

--- index.php
+++ index.php
@@ -41,17 +41,22 @@
 
          $mapper->id=$data->id; //azonosító
          $mapper->date=$data->date; //dátum
-         $mapper->projectname=$data->projectname; //projekt neve
-         $mapper->type=$data->type; //típus
-         $mapper->amountshift1=$data->amountshift1; // tervezett darab szám reggeles szak
-         $mapper->amountshift2=$data->amountshift2; // tervezett darab szám délutános szak
-         $mapper->amountshift3=$data->amountshift3; // tervezett darab szám éjszakás szak
+         $mapper->eventtype=$data->eventtype; //esettípus
          $mapper->description=$data->description; // részletes leírás
-         $mapper->attempt=$data->attempt; //próbálkozás
-         $mapper->whogo=$data->whogo; //ki megy
-         $mapper->where=$data->where; //hova megy
-         $mapper->period=$data->period; //mennyi ideig
-         $mapper->person=$data->person; //hány főre van szükség
+         if($data->eventtype=="újrateszt"){
+            $mapper->projectname=$data->projectname; //projekt neve
+            $mapper->type=$data->type; //típus
+            $mapper->amountshift1=$data->amountshift1; // tervezett darab szám reggeles szak
+            $mapper->amountshift2=$data->amountshift2; // tervezett darab szám délutános szak
+            $mapper->amountshift3=$data->amountshift3; // tervezett darab szám éjszakás szak
+         }
+         else if($data->eventtype=="kísérlet"){
+            $mapper->attempt=$data->attempt; //próbálkozás
+            $mapper->whogo=$data->whogo; //ki megy
+            $mapper->where=$data->where; //hova megy
+            $mapper->period=$data->period; //mennyi ideig
+            $mapper->person=$data->person; //hány főre van szükség
+         }
          $mapper->save();
 
          echo "OK";
@@ -68,17 +73,22 @@
          $mapper=new \DB\Jig\Mapper($db,'plans.json');
          $plan=$mapper->load(Array('@id=?',$params['id']));
 
-         $plan->projectname=$data->projectname;
-         $plan->type=$data->type;
-         $plan->amountshift1=$data->amountshift1;
-         $plan->amountshift2=$data->amountshift2;
-         $plan->amountshift3=$data->amountshift3;
-         $plan->description=$data->description;
-         $plan->attempt=$data->attempt;
-         $plan->whogo=$data->whogo;
-         $plan->where=$data->where;
-         $plan->period=$data->period;
-         $plan->person=$data->person;
+         if($plan->eventtype=="újrateszt"){
+            $plan->projectname=$data->projectname;
+            $plan->type=$data->type;
+            $plan->amountshift1=$data->amountshift1;
+            $plan->amountshift2=$data->amountshift2;
+            $plan->amountshift3=$data->amountshift3;
+            $plan->description=$data->description;
+         }
+         else if($plan->eventtype=="kísérlet"){
+            $plan->attempt=$data->attempt;
+            $plan->whogo=$data->whogo;
+            $plan->where=$data->where;
+            $plan->period=$data->period;
+            $plan->person=$data->person;
+            $plan->description=$data->description;
+         }
          $plan->save();
 
          echo "OK";
